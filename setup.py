5d035ce14d351ead9f994899e413efcc4f1e5393
GI_Jack <GI_Jack@hackermail.com>
2021-01-27 16:54:36 -0500

    added new icon sizes: 48, 72, 96

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup
 import os
+import sys
 import shutil
 
-# Generate directory for icons
-os.mkdir('icons')
-size_list = ['32','64','128','256','512']
+added_files = []
+size_list = ['32', '48', '64', '72', '96', '128', '256', '512']
+if 'linux' in sys.platform:
+    # Generate directory for icons
+    os.mkdir('icons')
 
-icon_files = []
-subdir = ""
-for size in size_list:
-    subdir  = 'icons/' + size
-    in_file = 'desktop/tor-util-' + size + '.png'
-    os.mkdir(subdir)
-    shutil.copy(in_file, subdir + '/tor-util.png')
-    out_dir = 'share/icons/hicolor/' + size + "x" + size + "/apps/"
-    entry = (out_dir, [subdir + '/tor-util.png'] )
-    icon_files.append(entry)
+    subdir = ""
+    # Icons
+    for size in size_list:
+        subdir  = 'icons/' + size
+        in_file = 'desktop/tor-util-' + size + '.png'
+        os.mkdir(subdir)
+        shutil.copy(in_file, subdir + '/tor-util.png')
+        out_dir = 'share/icons/hicolor/' + size + "x" + size + "/apps/"
+        entry   = (out_dir, [subdir + '/tor-util.png'] )
+        added_files.append(entry)
 
-icon_files.append(('share/tor-util',['tor-util.ui']))
+    added_files.append(('share/tor-util/'    , ['tor-util.ui']))
+    added_files.append(('share/applications/', ['desktop/tor_util.desktop']))
 
 setup(name='tor_util',
       version='0.1.0',
@@ -28,7 +32,7 @@ setup(name='tor_util',
       packages=['tor_util'],
       license='GPLv3',
       scripts=['tor_util_cli.py', 'tor_util_gui.py'],
-      data_files=icon_files,
+      data_files=added_files,
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
