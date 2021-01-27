2c055eb0f2bec483f84ed2cc5dcb049cf5adf3d7
GI_Jack <GI_Jack@hackermail.com>
2021-01-27 16:23:48 -0500

    removing file I do not use at this time. If I manage this project with QT Creator I will recreate

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import setup
 import os
+import shutil
+
+# Generate directory for icons
+os.mkdir('icons')
+size_list = ['32','64','128','256','512']
+
+icon_files = []
+subdir = ""
+for size in size_list:
+    subdir  = 'icons/' + size
+    in_file = 'desktop/tor-util-' + size + '.png'
+    os.mkdir(subdir)
+    shutil.copy(in_file, subdir + '/tor-util.png')
+    out_dir = 'share/icons/hicolor/' + size + "x" + size + "/"
+    entry = (out_dir, [subdir + '/tor-util.png'] )
+    icon_files.append(entry)
+
+icon_files.append(('share/tor-util',['tor-util.ui']))
 
 setup(name='tor_util',
       version='0.1.0',
@@ -10,7 +28,7 @@ setup(name='tor_util',
       packages=['tor_util'],
       license='GPLv3',
       scripts=['tor_util_cli.py', 'tor_util_gui.py'],
-      data_files = [('share/tor-util',['tor-util.ui'])],
+      data_files=icon_files,
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
@@ -21,6 +39,9 @@ setup(name='tor_util',
           'Operating System :: POSIX',
           'Programming Language :: Python',
           'Topic :: Utilities',
+          'Topic :: System :: Networking',
       ],
       install_requires = ['PyQt5', 'stem']
      )
+
+shutil.rmtree('icons')
