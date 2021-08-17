88d23f387e3d8efece910d85288853494cc7b7a8
GI_Jack <GI_Jack@hackermail.com>
2021-08-17 11:12:44 -0400

    added print() statement

--- gen_py_qrc.py
+++ gen_py_qrc.py
@@ -3,5 +3,6 @@
 # Helper script that compiles the Qt Resources file into an importable python
 # module
 import subprocess
+print("Compiling tor-util icon file")
 subprocess.call("rcc -g python tor-util.qrc > tor_util/tor_util_qrc.py",shell=True)
 subprocess.call("sed -i s/PySide2/PyQt5/g tor_util/tor_util_qrc.py",shell=True)
