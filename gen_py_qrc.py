bbb7e3558887181a930b1c6200adedd2bb07ef1b
GI_Jack <GI_Jack@hackermail.com>
2021-08-17 15:57:22 -0400

    replace sed with native python, because its guarunteed to work. Not all platforms have sed

--- gen_py_qrc.py
+++ gen_py_qrc.py
@@ -5,4 +5,16 @@
 import subprocess
 print("Compiling tor-util icon file")
 subprocess.call("rcc -g python tor-util.qrc > tor_util/tor_util_qrc.py",shell=True)
-subprocess.call("sed -i s/PySide2/PyQt5/g tor_util/tor_util_qrc.py",shell=True)
+
+## Replace PySide with PyQT, because we use PyQt in this project
+
+#subprocess.call("sed -i s/PySide2/PyQt5/g tor_util/tor_util_qrc.py",shell=True)
+# sed re-implemented natively in python to avoid compatibility issues
+# with platforms that don't support sed natively.
+f = open("tor_util/tor_util_qrc.py","r")
+contents = f.read()
+f.close()
+contents = contents.replace("PySide2","PyQt5")
+f =  open("tor_util/tor_util_qrc.py","w")
+f.write(contents)
+f.close()
