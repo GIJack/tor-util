e73ae64939a241e0baa72c704cee33240e13f62a
GI_Jack <GI_Jack@hackermail.com>
2021-08-17 16:59:40 -0400

    now with no shell calls, and no intermediate read/write.

--- gen_py_qrc.py
+++ gen_py_qrc.py
@@ -4,16 +4,10 @@
 # module
 import subprocess
 print("Compiling tor-util icon file")
-subprocess.call("rcc -g python tor-util.qrc > tor_util/tor_util_qrc.py",shell=True)
 
-## Replace PySide with PyQT, because we use PyQt in this project
-
-#subprocess.call("sed -i s/PySide2/PyQt5/g tor_util/tor_util_qrc.py",shell=True)
-# sed re-implemented natively in python to avoid compatibility issues
-# with platforms that don't support sed natively.
-f = open("tor_util/tor_util_qrc.py","r")
-contents = f.read()
-f.close()
+contents = subprocess.check_output(["rcc", "-g", "python", "tor-util.qrc"])
+contents = contents.decode()
+# Replace PySide with PyQT
 contents = contents.replace("PySide2","PyQt5")
 f =  open("tor_util/tor_util_qrc.py","w")
 f.write(contents)
